f38789aecba342a105a03d5f183a81b56211cb97
unknown <maleekbakare028@gmail.com>
2022-11-21 16:20:13 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include("page/head.php"); ?>
+   
+   <title>Document</title>
+</head>
+<body>
+    <link rel="stylesheet" href="link/css/style.css">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="box">
+                    <img src="img/pick.jpg" class="responsive mt-2" width="150">
+                    <div class="row">
+                        <div class="col-lg-6">
+                    <marquee>MY Portfo<span>lio</span>.....</marquee>
+                    </div>
+
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+
+
+            <div class="col-lg-4">
+                <div class="container">
+                    <div class="nav">
+                        <ul>
+                            <li><a href="#">HOME</a></li>
+                            <li><a href="#about">ABOUT</a></li>
+                            <li><a href="#ser">Service</a></li>
+                            <li><a href="#">Skills</a></li>
+                            <li><a href="#">Contact Us</a></li>
+
+
+                        </ul>
+
+
+                    </div>
+
+
+
+
+                </div>
+
+
+
+
+
+            </div>
+
+
+            <div class="col-lg-4">
+                <a href="#" class="btn">Subscribe <i class="fa-solid fa-bell" id="on"></i></a>
+                <p><span class="boo"><i class="fa-brands fa-facebook"></i> <i class="fa-brands fa-whatsapp"></i> <i class="fa-brands fa-instagram "></i></span></p>
+
+            </div>
+
+
+
+        </div>
+
+
+
+
+    </div>
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+        <div class="mine">
+            <p class="name"><span class="me">Hello,</span> my name is</p>
+            <h1 class="header mt-4">ABUBAKAR <span class="me">ABDULMALIK</span></h1>
+            <h2 class="fee mt-4">i'am a <span class="me"> Web</span> Developer</h2>
+            <form>
+                <input class="enter mt-2" type="email" name="email" id="mail" placeholder="enter your email">
+                <p><input class="time mt-4" type="submit" name="submit" value="Lets Start"></p>
+            </form>
+
+
+
+        </div>
+
+        </div>
+        </div>
+
+
+
+    </div>
+
+
+
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+        <h3>ABOUT ME</h3>CV
+        </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-7">
+                <img src="img/mine.JPG" class="Responsive mb-4">
+
+
+            </div>
+
+            <div class="col-lg-5">
+                <div id="#about">
+                    <p class="aa">I'm a front-end and back-end web developer.I can provide clean code and pixel
+                        perfect design.I also make the website more & more interactive with Web
+                         animations.I can provide clean code and pixel perfect design.
+                         A responsible design makes your website accessible to all users,regardless of their device. </p>
+
+                         <button class=" btn btn-outline-danger mb-5" style="margin-right: 25%;">
+                            <a style="color: white; text-decoration: none;"  href="https://wa.me/message/U2PUECDOAXEWD1">Let's Talk</a></button>
+
+
+
+
+                </div>
+
+
+
+
+            </div>
+
+
+
+        </div>
+
+
+
+
+
+
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+        <h3 id="ser">OUR SERVICE</h3>
+        </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="card mb-5">
+                    <div class="opp">
+                    <h2 class="new">Business website  <i class="fas fa-bars mt-4"></i>
+                    </h2>
+
+                    </div>
+                    <div class="image">
+                        <img src="img/As.png " width="100%">
+
+
+
+                    </div>
+                    <hr style="color: white;">
+                    <p class="read mt-2">
+                        We can provide a well responsible Business website across
+                        all media.
+                    </p>
+
+                   
+
+
+
+                </div>
+
+
+
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="opp">
+                    <h2 class="new">School website  <i class="fa-solid fa-school mt-4"></i></h2>
+
+                    </div>
+
+                    <div class="image">
+                        <img src="img/a.jpg" width="100%" height="300px" style="border-radius: 20px;" >
+
+
+
+                    </div>
+                    <hr style="color: white;">
+                    <p class="read mt-2">
+                        We can provide a well responsible school website across
+                        all media.
+                    </p>
+
+                   
+
+
+
+                </div>
+
+
+
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="opp">
+                    <h2 class="new">Ecommerce website  <i class="fas fa-shirt mt-4"></i></h2>
+
+                    </div>
+
+                    <div class="image">
+                        <img src="img/y.jpg" width="100%" height="300px" style="border-radius: 20px;" >
+
+
+
+                    </div>
+                    <hr style="color: white;">
+                    <p class="read mt-2">
+                        We can provide a well responsible Ecommerce website across
+                        all media.
+                    </p>
+
+
+
+                </div>
+
+
+
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <div class="opp">
+                    <h2 class="new">Marketing website  <i class="fas fa-bank mt-4"></i></h2>
+
+                    </div>
+
+                    <div class="image">
+                        <img src="img/kk.JPG " width="100%" style="border-radius: 20px;">
+
+
+
+                    </div>
+                    <hr style="color: white;">
+                    <p class="read mt-2">
+                        We can provide a well responsible Marketing website across
+                        all media.
+                    </p>
+
+
+
+                </div>
+
+
+
+            </div>
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+    <div class="container-fluid w-100 bg-dark">
+        <p  class="cop pt-2">COPYRIGHT@ 2020-2022</p>
+        <p class="cop">Powered By: <img src="img/pick.jpg" class="img-responsive" style="width:150px;"></p>
+        <p class="cop">Email: maleekbakare@gmail.com</p>
+        <p class="cop">Contact <i class="fa fa-phone"></i> 07085189810</p>
+      
+        
+      
+      
+      
+      </div>
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+</body>
+<script src="link/js/index.js"></script>
+<script src="link/jr/all.min.js"></script>
+<script src="link/js/bootstrap.min.js"></script>
+<script src="link/jv/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+ crossorigin="anonymous"></script>
+</html>
